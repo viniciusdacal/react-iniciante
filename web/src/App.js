3b804b4c35db376cca32258da054e13909628281
Vinicius Dacal <viniciusldacal@gmail.com>
2020-03-02 01:35:43 -0300

    componente app convertido para function

--- web/src/App.js
+++ web/src/App.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [showImage, setShowImage] = useState(false);
 
-    this.state = {
-      showImage: false,
-    };
+  const toggle = () => {
+    setShowImage(!showImage);
   }
 
-  toggle = () => {
-    this.setState({
-      showImage: !this.state.showImage,
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button type="button" onClick={this.toggle}>
-            {this.state.showImage ? 'Esconder' : 'Mostrar'}
-          </button>
-          {this.state.showImage && (
-            <img src={logo} className="App-logo" alt="logo" />
-          )}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button type="button" onClick={toggle}>
+          {showImage ? 'Esconder' : 'Mostrar'}
+        </button>
+        {showImage && (
+          <img src={logo} className="App-logo" alt="logo" />
+        )}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
