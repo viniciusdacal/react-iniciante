0e240d10f1c8c938b5f868b0cc530c5f7ab4512f
Vinicius Dacal <viniciusldacal@gmail.com>
2020-04-12 18:49:24 -0300

    implement api and connect interface with api

--- web/src/pages/Promotion/Search/Search.js
+++ web/src/pages/Promotion/Search/Search.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PromotionCard from 'components/Promotion/Card/Card';
-const promotion = {
-  "id": 1,
-  "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
-  "price": 1799,
-  "imageUrl": "https://cdn.gatry.com/gatry-static/promocao/imagem_url/2631517face1861bc4f46ae154d387de.png",
-  "url": "https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Mem%C3%B3ria/dp/B07YDWLV7S/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=gatry0b-20&linkId=e4a1146599e36741a720a6a952cbc328&language=pt_BR",
-  "comments": [
-    {
-      "id": 1,
-      "comment": "TELA HD"
-    }
-  ]
-};
+import axios from 'axios';
 
 const PagesPromotionSearch = () => {
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/promotions?_embed=comments')
+      .then((response) => {
+        setPromotions(response.data);
+      });
+  }, []);
+
   return (
     <div
       style={{
@@ -22,7 +19,9 @@ const PagesPromotionSearch = () => {
         margin: '30px auto',
       }}
     >
-      <PromotionCard promotion={promotion} />
+      {promotions.map((promotion) => (
+        <PromotionCard promotion={promotion} />
+      ))}
     </div>
   );
 };
