5cde8cdd578fb3c8dce6ddc9e9dc41d9f5c42449
Vinicius Dacal <viniciusldacal@gmail.com>
2020-06-07 00:02:13 -0300

    implement useApi hook

--- web/src/components/Promotion/Search/Search.js
+++ web/src/components/Promotion/Search/Search.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import useApi from 'components/utils/useApi';
 import { Link } from 'react-router-dom';
 
 import PromotionList from '../List/List';
 import './Search.css';
 
 const PromotionSearch = () => {
-  const [promotions, setPromotions] = useState([]);
   const [search, setSearch] = useState('');
+  const [load, loadInfo] = useApi({
+    url: '/promotions',
+    method: 'get',
+    params: {
+      _embed: 'comments',
+      _order: 'desc',
+      _sort: 'id',
+      title_like: search || undefined,
+    },
+  });
 
   useEffect(() => {
-    const params = {};
-    if (search) {
-      params.title_like = search;
-    }
-    axios.get('http://localhost:5000/promotions?_embed=comments&_order=desc&_sort=id', { params })
-      .then((response) => {
-        setPromotions(response.data);
-      });
+    load();
   }, [search]);
 
   return (
@@ -33,7 +35,11 @@ const PromotionSearch = () => {
         value={search}
         onChange={(ev) => setSearch(ev.target.value)}
       />
-      <PromotionList promotions={promotions} loading={!promotions.length} />
+      <PromotionList
+        promotions={loadInfo.data}
+        loading={loadInfo.loading}
+        error={loadInfo.error}
+      />
     </div>
   );
 };
